f65c22be34718d0758d9c4c4ac1a606de54c2997
ronardj01 <ronardj01@gmail.com>
2021-07-05 12:31:33 +0200

    Added endpoint to create a new hotel

--- api/server.js
+++ api/server.js
@@ -2,8 +2,11 @@ const express = require('express');
 const app = express();
 const { Pool } = require('pg');
 
+app.use(express.json());
+
 //queries
 const allHotels = 'select * from hotels h';
+const newHotel = 'insert into hotels (name, rooms, postcode) values ($1, $2, $3)';
 
 //connection settings
 const pool = new Pool({
@@ -14,16 +17,29 @@ const pool = new Pool({
   port: 5432
 });
 
+//endpoints
 app.get('/hotels', function (req, res) {
   pool.query(allHotels, (err, result) => {
-    if(err) {
+    if (err) {
       return console.error('Error executing query', err.stack)
     }
     return res.json(result.rows)
   })
 });
 
-app.listen(3000, function() {
+app.post('/hotels', function (req, res) {
+  const { name, rooms, postcode } = req.body;
+  const values = [name, rooms, postcode];
+
+  pool.query(newHotel, values, (err, result) => {
+    if (err) {
+      return console.error('Error executing query', err.stack)
+    }
+    return res.send('Hotel created!!!')
+  })
+});
+
+app.listen(3000, function () {
   console.log('Server is listening on port 3000')
 });
 
