53bc70a27309113e5eefb057d6c29cb035d96c04
ronardj01 <ronardj01@gmail.com>
2021-07-05 14:08:20 +0200

    Created some validations to create a new hotel

--- api/server.js
+++ api/server.js
@@ -7,6 +7,7 @@ app.use(express.json());
 //queries
 const allHotels = 'select * from hotels h';
 const newHotel = 'insert into hotels (name, rooms, postcode) values ($1, $2, $3)';
+const retrieveHotelName = 'select * from hotels h where name = $1'
 
 //connection settings
 const pool = new Pool({
@@ -31,12 +32,24 @@ app.post('/hotels', function (req, res) {
   const { name, rooms, postcode } = req.body;
   const values = [name, rooms, postcode];
 
-  pool.query(newHotel, values, (err, result) => {
-    if (err) {
-      return console.error('Error executing query', err.stack)
+  const invalidRoomsNumber = !Number.isInteger(rooms) || rooms <= 0;
+
+  if (invalidRoomsNumber) {
+    return res.status(400).send('"The number of rooms should be a positive integer"')
+  }
+  !(async function () {
+    const client = await pool.connect();
+    let result = await client.query(retrieveHotelName, [name]);
+
+    if (result.rowCount > 0) {
+      client.release();
+      return res.status(400).send('An hotel with the same name already exists!');
+    } else {
+      result = await client.query(newHotel, values);
+      client.release();
+      return res.send('Hotel Created!');
     }
-    return res.send('Hotel created!!!')
-  })
+  })();
 });
 
 app.listen(3000, function () {
